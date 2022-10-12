e02a3dc782ecc57f22052957e554f9248446f055
fredj789 <fredj_jomaa@hotmail.com>
2022-10-12 19:08:12 +0200

    Programming  Code flutter from Widget

--- lib/tuto.dart
+++ lib/tuto.dart
@@ -15,19 +15,48 @@ class _MyTestState extends State<MyTest> {
         appBar: AppBar(),
         drawer: Drawer(),
         body: Container(
-            padding: EdgeInsets.all(10),
-            child: GridView(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
-              children: List.generate(user.length, (index) {
-                // LE NOMBRE DE CONTAINNER
-                return Container(
-                  child: Text("${user[index]}"),
+          padding: EdgeInsets.all(10),
+          child: GridView.count(
+              crossAxisCount: 2, //le nombre de colone
+              crossAxisSpacing: 3, // espaace entre container
+              children: [
+                Container(
                   color: Colors.red,
-                  height: 100,
-                );
-              }),
-            )));
+                  height: 200,
+                  child: Text(
+                    "Container one",
+                  ),
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                  child: Text(
+                    "Container two",
+                  ),
+                ),
+                Container(
+                  color: Colors.yellowAccent,
+                  height: 200,
+                  child: Text(
+                    "Container three",
+                  ),
+                ),
+                Container(
+                  color: Colors.amber,
+                  height: 200,
+                  child: Text(
+                    "Container for",
+                  ),
+                ),
+                Container(
+                  color: Colors.brown,
+                  height: 200,
+                  child: Text(
+                    "Container five",
+                  ),
+                ),
+              ]),
+        ));
   }
 }
 //Liste Generate ==> Loop LIKE WHILE FOR DO  WHILE
