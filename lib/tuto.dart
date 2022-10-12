1b896d034e737b224b49e6fd02efeb1005766693
fredj789 <fredj_jomaa@hotmail.com>
2022-10-12 19:50:26 +0200

    Programming  Code flutter from Widget

--- lib/tuto.dart
+++ lib/tuto.dart
@@ -10,53 +10,27 @@ class MyTest extends StatefulWidget {
 class _MyTestState extends State<MyTest> {
   @override
   Widget build(BuildContext context) {
-    List user = ["Aman allah", "Fredj ", "Molka", "Fatma"];
     return Scaffold(
-        appBar: AppBar(),
-        drawer: Drawer(),
+        appBar: AppBar(
+          title: Text("Home Page"),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {},
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.alarm)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app)),
+          ],
+          elevation: 20, // l'ombre
+          shadowColor: Colors.red, // le couleur de l 'ombre
+          backgroundColor: Colors.green, // This color of the appbar
+          brightness: Brightness.dark, // le bar ou se trouve le batterie
+          centerTitle: true, // le titre de app est au centre
+        ),
+        //drawer: Drawer(),
         body: Container(
-          padding: EdgeInsets.all(10),
-          child: GridView.count(
-              crossAxisCount: 2, //le nombre de colone
-              crossAxisSpacing: 3, // espaace entre container
-              children: [
-                Container(
-                  color: Colors.red,
-                  height: 200,
-                  child: Text(
-                    "Container one",
-                  ),
-                ),
-                Container(
-                  color: Colors.blue,
-                  height: 200,
-                  child: Text(
-                    "Container two",
-                  ),
-                ),
-                Container(
-                  color: Colors.yellowAccent,
-                  height: 200,
-                  child: Text(
-                    "Container three",
-                  ),
-                ),
-                Container(
-                  color: Colors.amber,
-                  height: 200,
-                  child: Text(
-                    "Container for",
-                  ),
-                ),
-                Container(
-                  color: Colors.brown,
-                  height: 200,
-                  child: Text(
-                    "Container five",
-                  ),
-                ),
-              ]),
-        ));
+            padding: EdgeInsets.all(10),
+            child: Text("My name is fredj jomaa")));
   }
 }
-//Liste Generate ==> Loop LIKE WHILE FOR DO  WHILE
+//
