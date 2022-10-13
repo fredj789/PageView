48a1f2f98c459b26eede34356c2597091c616ba6
fredj789 <fredj_jomaa@hotmail.com>
2022-10-13 08:47:46 +0200

    Programming Code  Flutter

--- lib/tuto.dart
+++ lib/tuto.dart
@@ -12,10 +12,40 @@ class _MyTestState extends State<MyTest> {
   Widget build(BuildContext context) {
     return DefaultTabController(
         length:
-            2, // cette nombre est identique au nombre de  whidget tabBar View
+            3, // cette nombre est identique au nombre de  whidget tabBar View
         child: Scaffold(
             appBar: AppBar(
               title: Text("Home Page"),
+              bottom: TabBar(
+                  //isscroller permet de activer le scroll dans app bar
+                  isScrollable: true,
+                  // le couleur de trais tab bar
+                  indicatorColor: Colors.white,
+                  // this size trait  for Tab bar
+                  indicatorWeight: 2,
+                  // LE COULEUR DE LABEL WHGDGET
+                  labelColor: Colors.white,
+                  //indicatorPadding: EdgeInsets.all(5),
+                  labelStyle: TextStyle(fontSize: 12),
+                  tabs: [
+                    // le nombre de tab elle sera egal au nombre de whidget
+                    Tab(
+                      child: Text("Widget one"),
+                      icon: Icon(Icons.person),
+                    ),
+                    Tab(
+                      child: Text("Widget Twe"),
+                      icon: Icon(Icons.home),
+                    ),
+                    Tab(
+                      child: Text("Widget Tree"),
+                      icon: Icon(Icons.perm_camera_mic),
+                    ),
+                    Tab(
+                      child: Text("Widget for"),
+                      icon: Icon(Icons.percent_sharp),
+                    ),
+                  ]),
             ),
             drawer: Drawer(),
             body: TabBarView(
@@ -33,6 +63,26 @@ class _MyTestState extends State<MyTest> {
                 Container(
                   width: double.infinity,
                   color: Colors.blue,
+                  child: Column(children: [
+                    Text("Text 3"),
+                    Text("Text 4"),
+                    Text("Text 5"),
+                    Text("Text 6"),
+                  ]),
+                ),
+                Container(
+                  width: double.infinity,
+                  color: Colors.brown,
+                  child: Column(children: [
+                    Text("Text 3"),
+                    Text("Text 4"),
+                    Text("Text 5"),
+                    Text("Text 6"),
+                  ]),
+                ),
+                Container(
+                  width: double.infinity,
+                  color: Colors.white,
                   child: Column(children: [
                     Text("Text 3"),
                     Text("Text 4"),
